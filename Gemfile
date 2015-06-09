d03b3ad32ca88d17d37228ac5b1971f2624f2cc2
cema-sp <cema.rus@gmail.com>
2015-06-09 11:25:36 +0300

    Adding blog template

--- Gemfile
+++ Gemfile
@@ -1,23 +1,9 @@
-source "https://rubygems.org"
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
 
-gem "middleman", "~> 3.2.0"
-# gem "middleman-deploy"
-# gem "middleman-favicon-maker", "~> 3.2"
+gem "middleman", "~> 3.3.10"
+gem "middleman-blog", "~> 3.5.3"
 
-gem "compass", "~> 0.12.2"
-
-# Compass plugins
-gem "susy", "~> 1.0.9"
-gem "compass-h5bp", "~> 0.1.2" # HTML5 Boilerplate styles
-
-# Font Awesome icons
-gem "font-awesome-middleman", "~> 4.0.3"
-
-# Javascript
-# gem "therubyracer"
-# gem "therubyrhino"
-
-### Windows specific gems ###
-platforms :mswin, :mingw do
-   gem "wdm", "~> 0.1.0" # Windows Directory Monitor
-end
+# For feed.xml.builder
+gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,9 +1,39 @@
 ###
-# Compass
+# Blog settings
 ###
 
-# Susy grids in Compass
-require 'susy'
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
+# Compass
+###
 
 # Change Compass configuration
 # compass_config do |config|
@@ -11,92 +41,51 @@ require 'susy'
 # end
 
 ###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
+# Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
 
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
+
 # Methods defined in the helpers block are available in templates
-helpers do
-  # Calculate the years for a copyright
-  def copyright_years(start_year)
-    end_year = Date.today.year
-    if start_year == end_year
-      start_year.to_s
-    else
-      start_year.to_s + '-' + end_year.to_s
-    end
-  end
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
 
-  # Holder.js image placeholder helper
-  def img_holder(opts = {})
-    return "Missing Image Dimension(s)" unless opts[:width] && opts[:height]
-    return "Invalid Image Dimension(s)" unless opts[:width].to_s =~ /^\d+$/ && opts[:height].to_s =~ /^\d+$/
+set :css_dir, 'stylesheets'
 
-    img  = "<img data-src=\"holder.js/#{opts[:width]}x#{opts[:height]}/auto"
-    img << "/#{opts[:bgcolor]}:#{opts[:fgcolor]}" if opts[:fgcolor] && opts[:bgcolor]
-    img << "/text:#{opts[:text].gsub(/'/,"\'")}" if opts[:text]
-    img << "\" width=\"#{opts[:width]}\" height=\"#{opts[:height]}\">"
+set :js_dir, 'javascripts'
 
-    img
-  end
-
-end
-
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
-
-# Change the fonts directory
-# set :fonts_dir,  "alternative_fonts_directory"
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-
-  # Requires middleman-favicon-maker
-  # activate :favicon_maker,
-  #   :favicon_maker_base_image => "favicon_base.svg"
-
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
@@ -104,31 +93,11 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
-
-# Requires middleman-deploy and rsync
-# activate :deploy do |deploy|
-#   deploy.method = :rsync
-#   deploy.user   = "example"
-#   deploy.host   = "www.example.com"
-#   deploy.path   = "/public_html"
-#   # Optional Settings
-#   deploy.port  = 22 # ssh port, default: 22
-#   deploy.clean = true # remove orphaned files on remote host, default: false
-#   deploy.build_before = true # default: false
-# end
-
-# Skip locale validation (and validation warnings)
-I18n.enforce_available_locales = false
